7a83b74b71cb5250af1fd209acb3fb5457368049
Fahim Bagar <fahim.bagar@gmail.com>
2023-02-11 17:25:44 +0700

    Useless int

--- doc.go
+++ doc.go
@@ -1,3 +1,4 @@
-// This is just another useless module.
-
+/*
+Package just_useless_module is just another useless module.
+*/
 package just_useless_module
--- useless.go
+++ useless.go
@@ -1,8 +1,10 @@
-/*
-Useless Module.
-*/
+// Useless Module.
+
 package just_useless_module
 
 func String() string {
 	return "string"
+}
+func Int() int {
+	return 64
 }
